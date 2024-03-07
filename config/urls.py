869181ac4e240d6944c00084b841ac0aeca286fb
barabarinov <barabarinov@gmail.com>
2024-03-08 01:38:56 +0200

    formatted by black

--- config/urls.py
+++ config/urls.py
@@ -6,8 +6,10 @@ from django.urls import path, include, reverse_lazy
 from config import settings
 
 urlpatterns = [
-    path("", lambda request: redirect(reverse_lazy("movie:movie-list"), permanent=True)),
-    path("movies/", include('movie.urls')),
+    path(
+        "", lambda request: redirect(reverse_lazy("movie:movie-list"), permanent=True)
+    ),
+    path("movies/", include("movie.urls")),
     path("crew/", include("crew.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
