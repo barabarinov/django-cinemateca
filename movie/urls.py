1e1721b52148ba36288806c77b9ea4fcfd9f4a84
barabarinov <barabarinov@gmail.com>
2024-03-07 21:45:01 +0200

    fixed urls

--- movie/urls.py
+++ movie/urls.py
@@ -12,8 +12,8 @@ app_name = 'movie'
 
 urlpatterns = [
     path('', MovieListView.as_view(), name="movie-list"),
-    path("movies/<int:pk>/", MovieDetailView.as_view(), name='movie-detail'),
-    path("movies/create/", MovieCreateView.as_view(), name='movie-create'),
-    path("movies/update/<int:pk>/", MovieUpdateView.as_view(), name='movie-update'),
-    path("movies/delete/<int:pk>/", MovieDeleteView.as_view(), name='movie-delete'),
+    path("<int:pk>/", MovieDetailView.as_view(), name='movie-detail'),
+    path("create/", MovieCreateView.as_view(), name='movie-create'),
+    path("update/<int:pk>/", MovieUpdateView.as_view(), name='movie-update'),
+    path("delete/<int:pk>/", MovieDeleteView.as_view(), name='movie-delete'),
 ]
