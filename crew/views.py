07340dfae2ac1c901ec093c4ccbb6b9178e5e028
barabarinov <barabarinov@gmail.com>
2024-03-07 21:45:49 +0200

    added views

--- crew/views.py
+++ crew/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.views.generic import DetailView, CreateView, DeleteView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
-# Create your views here.
+from crew.forms import DirectorCreationForm
+from crew.models import Director
+
+
+class DirectorDetailView(LoginRequiredMixin, DetailView):
+    model = Director
+
+
+class DirectorCreateView(CreateView):
+    model = Director
+    form_class = DirectorCreationForm
+    template_name = "crew/director_form.html"
+    success_url = reverse_lazy("login")
+
+
+class DirectorUpdateView(LoginRequiredMixin, UpdateView):
+    model = Director
+
+
+class DirectorDeleteView(LoginRequiredMixin, DeleteView):
+    model = Director
+
+
+class ActorDetailView(LoginRequiredMixin, DetailView):
+    ...
+
+
+class ActorCreateView(LoginRequiredMixin, CreateView):
+    ...
+
+
+class ActorUpdateView(LoginRequiredMixin, UpdateView):
+    ...
+
+
+class ActorDeleteView(LoginRequiredMixin, DeleteView):
+    ...
