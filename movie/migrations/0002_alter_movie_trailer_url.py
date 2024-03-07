ff9da2fece8d6290d7bc47f21ac8f79775526ca8
barabarinov <barabarinov@gmail.com>
2024-03-08 01:30:17 +0200

    added migrations

--- movie/migrations/0002_alter_movie_trailer_url.py
+++ movie/migrations/0002_alter_movie_trailer_url.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('movie', '0001_initial'),
+        ("movie", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='movie',
-            name='trailer_url',
+            model_name="movie",
+            name="trailer_url",
             field=models.URLField(blank=True, null=True),
         ),
     ]
