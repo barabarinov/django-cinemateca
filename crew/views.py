0d35a17f36e36ad0f0187944d35d1b29976f43e0
barabarinov <barabarinov@gmail.com>
2024-03-08 01:39:39 +0200

    updated

--- crew/views.py
+++ crew/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
 from crew.forms import DirectorCreationForm
-from crew.models import Director
+from crew.models import Director, Actor
 
 
 class DirectorDetailView(LoginRequiredMixin, DetailView):
@@ -19,23 +19,32 @@ class DirectorCreateView(CreateView):
 
 class DirectorUpdateView(LoginRequiredMixin, UpdateView):
     model = Director
+    form_class = DirectorCreationForm
+    template_name = "crew/director_form.html"
+    success_url = reverse_lazy("movie:movie-list")
 
 
 class DirectorDeleteView(LoginRequiredMixin, DeleteView):
     model = Director
+    success_url = reverse_lazy("movie:movie-list")
 
 
 class ActorDetailView(LoginRequiredMixin, DetailView):
-    ...
+    model = Actor
 
 
 class ActorCreateView(LoginRequiredMixin, CreateView):
-    ...
+    model = Actor
+    success_url = reverse_lazy("movie:movie-list")
+    fields = "__all__"
 
 
 class ActorUpdateView(LoginRequiredMixin, UpdateView):
-    ...
+    model = Actor
+    success_url = reverse_lazy("movie:movie-list")
+    fields = "__all__"
 
 
 class ActorDeleteView(LoginRequiredMixin, DeleteView):
-    ...
+    model = Actor
+    success_url = reverse_lazy("movie:movie-list")
