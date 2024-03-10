ed418474284bd8983060fe0e4b2eb248c8847fc7
barabarinov <barabarinov@gmail.com>
2024-03-11 00:04:50 +0200

    changed conn_max_age=0

--- config/settings.py
+++ config/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=0)
 DATABASES["default"].update(db_from_env)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
