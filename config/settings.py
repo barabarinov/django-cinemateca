bece7965f6abb6dc59b188c01fd30d2da4cb8a28
barabarinov <barabarinov@gmail.com>
2024-03-10 19:27:21 +0200

    added host to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -18,7 +18,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = [
+    "127.0.0.1", "localhost", "django-cinemateca.onrender.com"
+]
 
 INTERNAL_IPS = [
     "127.0.0.1",
