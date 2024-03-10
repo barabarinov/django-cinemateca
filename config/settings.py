51785f2f3854e477c6cbd8ad1402d2c95e8a7f7e
barabarinov <barabarinov@gmail.com>
2024-03-10 17:15:28 +0200

    fixed DEBUG for deployment

--- config/settings.py
+++ config/settings.py
@@ -16,9 +16,7 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DJANGO_DEBUG = os.getenv("DJANGO_DEBUG", "") != "False"
-
-DEBUG = False
+DEBUG = "RENDER" not in os.environ
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
