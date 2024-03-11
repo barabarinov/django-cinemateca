5e0e96cd0a269e1607e82961e9fbf84cb3134a37
barabarinov <barabarinov@gmail.com>
2024-03-11 21:51:19 +0200

    added host for debug-toolbar for testing

--- config/settings.py
+++ config/settings.py
@@ -25,6 +25,7 @@ ALLOWED_HOSTS = [
 
 INTERNAL_IPS = [
     "127.0.0.1",
+    "django-cinemateca.onrender.com",
 ]
 
 # Application definition
--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,7 @@
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import path, include, reverse_lazy
 
-from config import settings
 
 urlpatterns = [
     path(
