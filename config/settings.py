65d399935e12c79baf48ef3cd2dc9c9610c93b97
barabarinov <barabarinov@gmail.com>
2024-03-11 09:51:59 +0200

    deleted MEDIA constants

--- config/settings.py
+++ config/settings.py
@@ -134,8 +134,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# MEDIA_URL = "/media/"
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 # Default primary key field type
